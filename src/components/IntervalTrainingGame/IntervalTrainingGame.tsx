47a26fce8989889cdfce247d6fb99d55fc819d6a
hsiehchenwei <hsiehchenwei@gmail.com>
2025-06-20 22:32:42 +0800

    fix bug

--- src/components/IntervalTrainingGame/IntervalTrainingGame.tsx
+++ src/components/IntervalTrainingGame/IntervalTrainingGame.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "../Button/Button";
 import RootNotePlayer from "../RootNotePlayer/RootNotePlayer";
 import NotePlayButton from "../NotePlayButton/NotePlayButton";
